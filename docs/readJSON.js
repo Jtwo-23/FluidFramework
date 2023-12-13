7ff980d0a43fcf29c100e59b11d9b7f3a0583562
Michael Zhen <112977307+zhenmichael@users.noreply.github.com>
2023-12-13 13:19:38 +0000

    test build docs

---
+++ docs/readJSON.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+// Read the JSON file content
+const jsonData = fs.readFileSync('docs/data/versions.json', 'utf8');
+
+// Parse the JSON data
+const parsedData = JSON.parse(jsonData);
+
+// Extract values from "previousVersions" and output for Bash processing
+console.log(parsedData.params.previousVersions.join('\n'));
